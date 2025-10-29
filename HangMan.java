a5895b04012a004d1179929c6ca92c0367748748
Navid Alvey <navidalvey@gmail.com>
2025-10-29 15:56:44 -0500

    Updated with guessing functionality

--- HangMan.java
+++ HangMan.java
@@ -10,30 +10,48 @@ public class HangMan {
 
         Scanner scan = new Scanner(System.in);
 
+        // Get word from user
         System.out.println("Enter a word");
-        String word = scan.next(); // get word Goofy
+        String word = scan.next(); 
         
+        // Create a character array named charArray and 
+        // convert the word String into a character array
         char[] charArray = word.toCharArray();
             for (char c : charArray) {
                 System.out.println(c);
             }
 
-
+        // Display " _ " for each character of the word    
         for (int i = 0; i < word.length(); i++) {
             System.out.print("_ ");
         }
         System.out.println();
 
-/* 
-        String guess;
-        try {
-            guess = scan.next();
-            if (guess.equals(word)) {
-                System.out.println("Hooray!!! You win :)");
-            } else if (guess.is)
-        } catch { 
+        // Accept a guessed letter
+        int numberOfGuessesRemaining = word.length();
+        char guess;
+        System.out.println("Guess a letter!");
+        guess = scan.next().charAt(0);
 
+        for (int j = 0; j < charArray.length; j++) {
+            try {
+                if (guess == (charArray[j])) {
+                    System.out.println("HOORAY! YOU GUESSED A LETTER!");  
+                    --numberOfGuessesRemaining;
+                } 
+            } catch (Exception e) {
+                System.out.println("The letter you guessed is not within the word :(");
+            }
         }
-        */
+
+        // Reduce possible guesses
+        
+        // Guessed a letter in word?
+
+        // No
+
+        // Yes
+
+        // Reduce removing guesses
     }
 }
