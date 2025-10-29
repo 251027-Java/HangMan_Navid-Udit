6263d0387dae0c44985e829bce42c1e42eed927d
Navid Alvey <navidalvey@gmail.com>
2025-10-29 15:20:58 -0500

    Added more to hangman

--- HangMan.java
+++ HangMan.java
@@ -10,8 +10,30 @@ public class HangMan {
 
         Scanner scan = new Scanner(System.in);
 
-        String word = "Goofy";
+        System.out.println("Enter a word");
+        String word = scan.next(); // get word Goofy
         
-        
+        char[] charArray = word.toCharArray();
+            for (char c : charArray) {
+                System.out.println(c);
+            }
+
+
+        for (int i = 0; i < word.length(); i++) {
+            System.out.print("_ ");
+        }
+        System.out.println();
+
+/* 
+        String guess;
+        try {
+            guess = scan.next();
+            if (guess.equals(word)) {
+                System.out.println("Hooray!!! You win :)");
+            } else if (guess.is)
+        } catch { 
+
+        }
+        */
     }
 }
