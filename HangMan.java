a2fd1ccb9872954c8e7fac1115c4213020a358d4
Navid Alvey <navidalvey@gmail.com>
2025-10-29 14:22:24 -0500

    Initial commit

---
+++ HangMan.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class HangMan {
+    public static void main(String[] args) {
+        // Create a word
+        // accept user input
+        // if user input is within chosen word print match
+        // if not, then draw make an askii art of a man
+        // repeat loop
+
+        Scanner scan = new Scanner(System.in);
+
+        String word = scan.nextLine();
+        next();
+    }
+}
